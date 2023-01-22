6909a7ddb9d108b705d541fda6c1c8cfe087c4e3
layz1008 <zlay1008@gmail.com>
2023-01-22 14:16:54 -0500

    Create Item function

--- src/screens/AddListing.jsx
+++ src/screens/AddListing.jsx
@@ -1,3 +1,61 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createItem } from "../services/items.js"
+
+export default function ItemCreate() {
+    const [item, setItem] = useState({
+        name: "",
+        image: "",
+        description: "",
+    });
+
+    let navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const createdItem = await createItem(item);
+        navigate(`/items/${createdItem.id}` , { replace: true });
+    };
+
+    const handleChange = (e) => {
+        const { value, name } = e.target;
+        setItem((prev) => ({
+            ...prev,
+            [name]: value
+
+        }))
+    };
+
+    return (
+        <div>
+        <h1>Add Item</h1>
+        <form className="create-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Item Name"
+            name="name"
+            value={item.name}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Image"
+            name="title"
+            value={item.image}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Item Description"
+            name="description"
+            value={item.description}
+            onChange={handleChange}
+          />
+          
+          <button type="submit">Free Item Listing</button>
+        </form>
+      </div>
+    );
+
+
+} 
