befcd6fb04927edd2cbfd4cff2427cf9d0278af0
ksooooza <ksoooza@gmail.com>
2023-01-22 17:40:17 -0500

    Adds some class names to home page for easier CSS later

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -9,16 +9,27 @@ export default function Home() {
     navigate("/items", { replace: true })
   }
   return (
-    <div>
+    <>
+    <div className="heading">
       <h1>HEY THERE! COME GET SOME FREE SHIT!</h1>
+      </div>
+      <div className="body">
       <p>Have you ever wanted to have some shit wihtout having to pay for that shit? Well now you can. Click below to see all of the free shit available, and click on an image to find out more details like who's giving it away and where you can pick it up. You ready to finally have some shit?!</p>
       <button onClick={handleNav}>Click to see some free shit</button>
+      </div>
+      <div className="devTeam">
       <h2>Meet your dev team</h2>
       {/* <img src="" alt="John"></img>
+      <p>A little blurby blurb about each of us.</p>
       <img src="" alt="Zack"></img>
+      <p>A little blurby blurb about each of us.</p>
       <img src="" alt="Justin"></img>
+      <p>A little blurby blurb about each of us.</p>
       <img src="" alt="Xavier"></img>
-      <img src="" alt="Katherine"></img> */}
+      <p>A little blurby blurb about each of us.</p>
+      <img src="" alt="Katherine"></img>
+      <p>A little blurby blurb about each of us.</p> */}
     </div>
+    </>
   )
 }
