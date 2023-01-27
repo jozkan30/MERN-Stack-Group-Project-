8aaeddab38c1e8265e6fc4130589c979bacd7aff
ValtierraXavier <xavier.valtierra@icloud.com>
2023-01-27 11:30:15 -0500

    Push to pull

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import './nav.css'
 import jwtDecode from "jwt-decode";
+import { signOut } from "../services/users";
 
 export default function Nav() {
  const userKey = localStorage.getItem('token')
 
+ function forceReload(){
+  window.location.reload(false)
+ }
  function handleLogOut(){
   localStorage.removeItem('token')
+  forceReload()
   userKey = null
  }
 
@@ -28,14 +33,13 @@ export default function Nav() {
         <NavLink className={"navBarInfo"} to="/add-listing">
           Add A New Item
         </NavLink>
-        
+
         {userKey === null ? 
         <div className="authButtons">
-       <NavLink className={"navBarInfo"} id="signinButton" to="/sign-in">Sign In</NavLink>
-        <NavLink className={"navBarInfo"} id="signupButton" to="/sign-up" >Sign Up</NavLink>
-        </div>
-        : <NavLink onClick={handleLogOut} className={"navBarInfo"} id="logOutButton" to="/">Log Out</NavLink>}
-        
+          <NavLink className={"navBarInfo"} id="signinButton" to="/sign-in">Sign In</NavLink>
+          <NavLink className={"navBarInfo"} id="signupButton" to="/sign-up" >Sign Up</NavLink>
+        </div>: 
+          <NavLink onClick={handleLogOut} className={"navBarInfo"} id="logOutButton" to="/">Log Out</NavLink>}
       </nav>
     );
   }
